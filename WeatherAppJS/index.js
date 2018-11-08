451bef18d5e6a2e6d221e76326e42dbdb6e7be3a
PMuiruri <35254271+PMuiruri@users.noreply.github.com>
2018-11-08 14:29:29 +0200

    fixed a cors bug

--- WeatherAppJS/index.js
+++ WeatherAppJS/index.js
@@ -1,46 +1,45 @@
 'use strict'
 
 let apiKey = '14c73a89eb6ba66911ae3b960533a3e9';
-let city = document.querySelector('input').value;
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`
-let input = document.querySelector('input');
+let city;
+
+//let input = document.querySelector('input');
 
 window.addEventListener('keydown', function(e){
 	let keyPressed= e.keyCode;
 	console.log(keyPressed);
 
 	if(keyPressed === 13){
-
-		window.location.reload();
+		fetchWeather();
 	}
-
 });
 
-			fetch(url)
-			.then(function(response) {
-			  if (!response.ok) {
-			    throw Error(response.statusText);
-			  }return response.json()})
-  		.then(function(myJson) {
-				renderData(myJson)
-			  // Do stuff with the response
-			})
-			.catch(function(error) {
-			  console.log('Looks like there was a problem: \n', error);
-				document.getElementById('error').innerText= "City Not Found";
-			});
+async function fetchWeather() {
+	try {
+		city = document.querySelector('input').value;
+		let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`;
+		const fetchResult = fetch(url)
 
-
-	function renderData(data){
-		console.log(data);
-		let description = data["weather"][0]["description"];
-		let icon = data["weather"][0]["icon"];
-		let currenttemp = data.main.temp;
-		let humidity = data.main.humidity;
-
-		document.getElementById('location').innerText=city;
-		document.getElementById('ico').src="http://openweathermap.org/img/w/"+icon+".png";
-		document.getElementById('desc').innerText = description;
-		document.getElementById('temp').innerHTML= currenttemp + "&deg;C";
-		// document.getElementById('humidity').innerText = humidity;
+		const response = await fetchResult;
+		const jsonData = await response.json();
+		renderData(jsonData);
+		}
+	catch(e){
+		console.log('Looks like there was a problem: \n', e);
+		document.getElementById("errors").innerText= "City Not found";
 	}
+}
+
+function renderData(data){
+	console.log(data);
+	let description = data["weather"][0]["description"];
+	let icon = data["weather"][0]["icon"];
+	let currenttemp = data.main.temp;
+	let humidity = data.main.humidity;
+
+	document.getElementById('location').innerText=data["name"];
+	document.getElementById('ico').src="https://openweathermap.org/img/w/"+icon+".png";
+	document.getElementById('desc').innerText = description;
+	document.getElementById('temp').innerHTML= currenttemp + "&deg;C";
+	// document.getElementById('humidity').innerText = humidity;
+}
